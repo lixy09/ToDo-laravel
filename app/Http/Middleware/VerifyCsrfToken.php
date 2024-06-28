79b05f690a7ff6b49954a4944eb67ea37daec3ea
lixy09 <li0047@hz.nl>
2024-06-28 23:06:02 +0200

    update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,5 +6,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-
+    protected $except = [
+        'payment/*', // exclude all URLs with payment/ prefix
+        'user/add' // exclude exact URL
+    ];
 }
