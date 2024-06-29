17099edb8225187167d68946748a20ba063067a9
lixy09 <li0047@hz.nl>
2024-06-29 21:33:47 +0200

    update env

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,14 @@
 import './bootstrap';
-
+import $ from 'jquery';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
-$(document).ready(function() {
-    $.ajaxSetup({
-        headers: {
-            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            "X-Requested-With": "XMLHttpRequest"
-        }
-    });
+$.ajaxSetup({
+    headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        "X-Requested-With": "XMLHttpRequest"
+    }
 });
