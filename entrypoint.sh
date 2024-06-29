8c08cefdaaa558f78eccca3a2f305c0d65598cdc
lixy09 <li0047@hz.nl>
2024-06-29 20:10:29 +0200

    update env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Copy the appropriate .env file
-if [[ -n "${IS_HEROKU}" ]]; then
-  echo "Using Heroku environment"
-  cp /var/www/html/.env.heroku /var/www/html/.env
-else
-  echo "Using local environment"
-  cp /var/www/html/.env /var/www/html/.env
-fi
+cp /var/www/html/.env /var/www/html/.env
 
 # Generate the application key
 php artisan key:generate --ansi
