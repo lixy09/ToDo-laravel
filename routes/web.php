91f5e560f1a189c8e516d0fcc143aa8a6ad7c997
lixy09 <li0047@hz.nl>
2024-06-29 20:13:24 +0200

    update env

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,15 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::middleware('auth')->group(function () {
-    Route::resource('profile', ProfileController::class);
-    Route::resource('tasks', TaskController::class);
-    Route::post('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
-});
+//Route::middleware('auth')->group(function () {
+//    Route::resource('profile', ProfileController::class);
+//    Route::resource('tasks', TaskController::class);
+//    Route::post('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+//});
+
+Route::resource('profile', ProfileController::class);
+Route::resource('tasks', TaskController::class);
+Route::post('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
 
 Route::get('/test-email', function () {
     \Illuminate\Support\Facades\Mail::raw('This is a test email', function ($message) {
