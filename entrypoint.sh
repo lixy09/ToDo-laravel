a0d57ec92448257fd9a66f78d37095f553419256
lixy09 <li0047@hz.nl>
2024-06-29 19:54:04 +0200

    update heroku.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Copy the appropriate .env file
+if [[ -n "${IS_HEROKU}" ]]; then
+  echo "Using Heroku environment"
+  cp /var/www/html/.env.heroku /var/www/html/.env
+else
+  echo "Using local environment"
+  cp /var/www/html/.env /var/www/html/.env
+fi
+
 # Generate the application key
 php artisan key:generate --ansi
 
@@ -7,8 +16,13 @@ php artisan key:generate --ansi
 php artisan config:clear
 php artisan config:cache
 
-# set the port
-export PORT=80
+# Setup storage
+php artisan storage:link
+
+# Dynamically set the Apache port (heroku sets it otherwise 80)
+if [[ -z "${PORT}" ]]; then
+  export PORT=80
+fi
 
 # Update the ports.conf file
 echo "Listen ${PORT}" > /etc/apache2/ports.conf
